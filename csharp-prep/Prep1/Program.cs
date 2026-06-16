db373114a1bad9983053c3692e69a768c3abf143
mtbigley <mtbigley@byui.edu>
2026-06-15 18:19:45 -0600

    attempt 1 at prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Get first name
+        Console.WriteLine("What is your first name? ");
+        string fname = Console.ReadLine();
+        // Get last name
+        Console.WriteLine("What is your last name? "); 
+        string lname = Console.ReadLine();
+
+        // print names in james bond order
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
