57a2ccc12211b1e2908bddf3388b244042eacc77
mtbigley <mtbigley@byui.edu>
2026-06-15 19:08:30 -0600

    Changed cutoff points for better precision (before it was grade = A if percentage was > than 89.9-- now it is A if percentage is >= 90)
    Changed variable type from int to double to be able to handle decimal point inputs like 79.9 as well

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,11 +8,11 @@ class Program
         Console.WriteLine("Hello, I'm a grades calculator designed to help you determine your letter grade and whether or not you passed your class!");
         
         // Retrieve and store percentage
-        Console.WriteLine("First things first, I'll need your final grade percentage.");
+        Console.WriteLine("First things first, I'll need your final grade percentage. Please input the grade percentage you achieved below:");
         string grade = Console.ReadLine();
 
         // Parse into useable integer
-        int gradePercentage = int.Parse(grade);
+        double gradePercentage = double.Parse(grade);
 
         // initialize final letter grade variable
         string finalGrade = "";
@@ -20,56 +20,56 @@ class Program
         // determine letter grade and message
 
         // A+ at 95% and up
-        if (gradePercentage > 94.9)
+        if (gradePercentage >= 95)
         {
             finalGrade = "A+";
             Console.WriteLine("You got an A+! Congrats on your hard work!");
         } 
 
         // A at 90% and up 
-        else if (gradePercentage > 89.9)
+        else if (gradePercentage >= 90)
         {
             finalGrade = "A";
             Console.WriteLine("You got an A! Give yourself a round of applause!");
         }
 
         // B+ at 85% and up
-        else if (gradePercentage > 84.9)
+        else if (gradePercentage >= 85)
         {
             finalGrade = "B+";
             Console.WriteLine("You got a B+! Good work!");
         }
 
         // B at 80% and up
-        else if (gradePercentage > 79.9)
+        else if (gradePercentage >= 80)
         {
             finalGrade = "B";
             Console.WriteLine("You got a B! Well done!");
         }
 
         // C+ at 75% and up
-        else if (gradePercentage > 74.9)
+        else if (gradePercentage >= 75)
         {
             finalGrade = "C+"; 
             Console.WriteLine("You got a C+. You passed!");
         }
 
         //  C at 70% and up 
-        else if (gradePercentage > 69.9)
+        else if (gradePercentage >= 70)
         {
             finalGrade = "C";
             Console.WriteLine("You got a C. You passed! Consider retaking or studying more to keep the concepts fresh and sharp for your next courses!");
         }
 
         // D+ at 65% and up
-        else if (gradePercentage > 64.9)
+        else if (gradePercentage >= 65)
         {
             finalGrade = "D+"; 
             Console.WriteLine("You got a D+. You'll have to retake the class but you're almost there!");
         }
 
         // D at 60% and up
-        else if (gradePercentage > 59.9)
+        else if (gradePercentage >= 60)
         {
             finalGrade = "D";
             Console.WriteLine("You got a D. You'll have to retake the class but you're almost there, just a little more study!");
@@ -83,6 +83,6 @@ class Program
         }
 
         // Print final letter grade for user
-        Console.WriteLine($"Your final letter grade came out to {finalGrade}");
+        Console.WriteLine($"Your final letter grade came out to: {finalGrade}.");
     }
 }
