a7c74e9501142aa39981361eaabec498e9aea86d
mtbigley <mtbigley@byui.edu>
2026-06-16 18:20:40 -0600

    Prep 4 commit 1-- program intro and list formation + user number gathering

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Introduce program 
+        Console.WriteLine("Hi! I'm a program to help you understand characterstics of a group of numbers.");
+        Console.WriteLine("I calculate the sum, average, and largest number of a list of numbers you provide.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // initialize list
+        List<double> numbers = new List<double>();
+
+        // initialize input variable 'number'
+        double number; 
+
+        // Loop while number entered != 0
+        do
+        {
+            // retrieve user number 
+            Console.Write("Enter number: ");
+            string numberEntered = Console.ReadLine(); 
+
+            // parse user number
+            number = double.Parse(numberEntered);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
     }
 }
