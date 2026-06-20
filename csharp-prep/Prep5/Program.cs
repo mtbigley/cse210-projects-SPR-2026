0f510324f0cf5361e0aee7c14230ae22eae5677c
mtbigley <mtbigley@byui.edu>
2026-06-20 16:16:40 -0600

    Prep 5 function 2

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,12 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Define welcome function
         void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
         }
 
+        // Define Username Prompt function
+        string PromptUserName()
+        {
+            // Store user input into string named Username
+            Console.Write("Please enter your name: ");
+            string Username = Console.ReadLine();
+            return Username; 
+        }
+
+
+
+
+        // Call functions 
         DisplayWelcome();
-        
+        PromptUserName(); 
     }
 }
