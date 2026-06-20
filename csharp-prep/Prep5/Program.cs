2f7239ecbe196f8ffd3f8f84cca5bba5d97b9894
mtbigley <mtbigley@byui.edu>
2026-06-20 16:08:38 -0600

    Prep 5 initial commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        DisplayWelcome();
+        
     }
 }
