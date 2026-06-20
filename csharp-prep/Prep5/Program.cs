85ab74eef0c8e58efb4d1fca4d52f06bf88259d9
mtbigley <mtbigley@byui.edu>
2026-06-20 16:24:13 -0600

    Prompt User number and User Birth year functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -16,14 +16,42 @@ class Program
             // Store user input into string named Username
             Console.Write("Please enter your name: ");
             string Username = Console.ReadLine();
+
             return Username; 
         }
 
+        // Define Favorite number prompt function
+        int PromptUserNumber()
+        {
+            // Store user input into string named FavoriteNumber
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine(); 
 
+            // parse FavoriteNumber
+            int FavoriteNumber = int.Parse(input);
+
+            return FavoriteNumber;
+        }
+
+        // Define Birth year prompt function
+        int PromptUserBirthYear()
+        {
+            // Store user input into string named BirthYear
+            Console.Write("Please enter the year you were born: ");
+            string input = Console.ReadLine(); 
+
+            // parse BirthYear
+            int BirthYear = int.Parse(input); 
+
+            return BirthYear;
+
+        }
 
 
         // Call functions 
         DisplayWelcome();
         PromptUserName(); 
+        PromptUserNumber();
+        PromptUserBirthYear(); 
     }
 }
