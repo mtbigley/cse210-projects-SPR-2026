4daaf99c7e1ea5c09d9371a6d83211bb4bc8b6f7
mtbigley <mtbigley@byui.edu>
2026-06-25 13:34:48 -0600

    Randomized fraction test and Final commit for now

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,16 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction defaultFraction = new Fraction(); 
+        Fraction defaultFraction = new Fraction(); // default test case
         Console.WriteLine(defaultFraction.GetFractionString()); 
         Console.WriteLine(defaultFraction.GetDecimalValue()); 
 
-        Fraction topFraction = new Fraction(16); 
+        Fraction topFraction = new Fraction(16); // constructor with numerator test case
         Console.WriteLine(topFraction.GetFractionString()); 
         Console.WriteLine(topFraction.GetDecimalValue()); 
 
-        Fraction fullFraction = new Fraction(16/64); 
+        Fraction fullFraction = new Fraction(16/64); // constructor with numerator and denominator test case 
         Console.WriteLine(fullFraction.GetFractionString());
         Console.WriteLine(fullFraction.GetDecimalValue()); 
+
+        Random random = new Random(); // randomized fraction test-- I've got some tweaks to make here regarding the number before it's good to go
+        Fraction randomFraction = new Fraction(); 
+        for (int i = 0; i <20; i++)
+        {
+            int topValue = random.Next(1,11); 
+            int bottomValue = random.Next(1,11); 
+            randomFraction.SetFractionTop(topValue); 
+            randomFraction.SetFractionBottom(bottomValue); 
+            Console.Write($"Fraction {i + 1}: "); 
+            Console.Write($"string: {randomFraction.GetFractionString()}"); 
+            Console.WriteLine($" Number: {randomFraction.GetDecimalValue()}"); 
+        }
     }
 }
