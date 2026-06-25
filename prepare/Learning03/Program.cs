dacf5ca2bbf51c361157c0bdf8e01bf475572046
mtbigley <mtbigley@byui.edu>
2026-06-25 13:28:19 -0600

    Test cases on Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction Test = new Fraction(); 
+        Fraction defaultFraction = new Fraction(); 
+        Console.WriteLine(defaultFraction.GetFractionString()); 
+        Console.WriteLine(defaultFraction.GetDecimalValue()); 
+
+        Fraction topFraction = new Fraction(16); 
+        Console.WriteLine(topFraction.GetFractionString()); 
+        Console.WriteLine(topFraction.GetDecimalValue()); 
+
+        Fraction fullFraction = new Fraction(16/64); 
+        Console.WriteLine(fullFraction.GetFractionString());
+        Console.WriteLine(fullFraction.GetDecimalValue()); 
     }
 }
