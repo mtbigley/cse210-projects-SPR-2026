14e7b92975ec3a6feeb79d7cd9585032ed6eb25e
mtbigley <mtbigley@byui.edu>
2026-06-25 14:46:10 -0600

    initial commit of file structure for classes and initial work on ScriptureClass

---
+++ prove/Develop03/ScriptureClass.cs
@@ -0,0 +1,14 @@
+using System; 
+
+public class Scripture()
+{
+    private string scriptureText; // all of the words in the scripture
+    private string reference; // the string form of the compiled reference including book, chapter and verse(s)
+
+    public Scripture() // constructor default placeholder scripture 
+    {
+        reference = " ooblar";
+        scriptureText = "ooblar"; 
+    }
+    
+}
