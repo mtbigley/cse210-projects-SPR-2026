cef4f8f9febd04d93682a843d3e53403109b14d6
mtbigley <mtbigley@byui.edu>
2026-06-27 20:59:30 -0600

    initial commit on Scripture class; finished constructor, GetDisplayText(), and HideRandomWords()

--- prove/Develop03/ScriptureClass.cs
+++ prove/Develop03/ScriptureClass.cs
@@ -1,14 +1,53 @@
 using System; 
 using System.Collections.Generic; 
-public class Scripture()
+public class Scripture
 {
     private List<Word> _words; // list of Word classes each with their own hidden/not hidden attributes. 
     private Reference _reference; // initialize reference class designed to contain book chapter and verse. 
 
-    public Scripture() // constructor default placeholder scripture 
+    public Scripture(Reference reference, string scriptureText) // default constructor
     {
-        reference = " ooblar";
-        scriptureText = "ooblar"; 
+        _reference = reference; // store the reference class into a variable
+
+        _words = new List<Word>(); // initialize list to store word objects
+
+        string[] words = scriptureText.Split(" "); // just learned about the .split method in c# this is super handy-- splits string into words and stores them in the list separated by spaces
+
+        foreach (string currentWord in words) // Pass each split word string into the Word class
+        {
+            Word newWord = new Word(currentWord); 
+
+            _words.Add(newWord); 
+        }
     }
+
+    public string GetDisplayText()
+    {
+        string displayText = ""; // initialize display string
+
+        displayText += _reference.GetDisplayText(); // add reference text and space
+        displayText += " "; 
+
+        foreach (Word word in _words) // add words and spaces to displaytext string 
+        {
+            displayText += word.GetDisplayText(); 
+            displayText += " ";
+        }
+
+        return displayText; // return final display text 
+    }
+
+    public void HideRandomWords() // hides 3 random words
+    {
+        Random random = new Random(); 
+
+        for (int i = 0; i < 3; i++)
+        {
+            int randomNumber = random.Next(_words.Count); 
+
+            _words[randomNumber].Hide(); 
+        }
+    }
+
     
 }
