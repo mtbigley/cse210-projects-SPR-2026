d91e44d379bea56e3cfd713ede509851f33cf7c0
mtbigley <mtbigley@byui.edu>
2026-06-27 21:23:20 -0600

    initial commit for Program.cs-- created scripture and reference using Luke 17:20-21 (my favorite scripture) and framework for user to quit program or for program to continue while words are unhidden.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
-        
+        Reference reference = new Reference("Luke", 17, 20-21); // create reference
+
+        // create scripture
+        Scripture scripture = new Scripture(reference, "And when he was demanded of the Pharisees when the kingdom of God should come he answered them and said The kingdom of God cometh not with observation Neither shall they say Lo here or lo there for behold the kingdom of God is within you");
+
+        while (!scripture.IsCompletelyHidden()) //Continue program until every word is hidden or until user inputs quit 
+        {
+            Console.Clear(); 
+
+            Console.WriteLine(scripture.GetDisplayText()); // call scripture display text 
+
+            Console.WriteLine(); 
+
+            Console.Write("Press Enter to continue or type 'quit': "); // prompt user input to continue or quit
+
+            string input = Console.ReadLine(); 
+
+            if (input == "quit")
+            {
+                break; 
+            }
+
+            scripture.HideRandomWords(); // hide random words
+        }
     }
 }
