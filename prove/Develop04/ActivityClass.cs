8f0613cc1c4b2231543f998fa1ddad031676d3ac
mtbigley <mtbigley@gmail.com>
2026-07-03 13:57:16 -0600

    Commit 2 for ActivityClass.cs-- need to figure out how to make a spinner or loading progress bar or some such

--- prove/Develop04/ActivityClass.cs
+++ prove/Develop04/ActivityClass.cs
@@ -4,7 +4,7 @@ public class Activity
 {
     private string _activityName; 
     private string _activityDescription;
-    private int _duration; // entered in seconds
+    private int _duration; // entered in seconds, convert later
 
     public Activity(string name, string description)
     {
@@ -15,8 +15,18 @@ public class Activity
 
     public void startActivity()
     {
+        // Intro message
         Console.Clear();
         Console.WriteLine($"Welcome to the {_activityName} activity.");
+        Console.WriteLine();
+        Console.WriteLine($"{_activityDescription}");
+        Console.WriteLine("How long would you like your activity session (in seconds) to last?");
+        string input = Console.ReadLine();
+        _duration = int.Parse(input);
+
+        Console.WriteLine();
+        Console.WriteLine("Starting activity...");
+        // loading bar goes here
         
     }
 }
