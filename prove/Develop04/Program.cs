6829ede96a35dbdace2a428c1bc60d94e7760556
mtbigley <mtbigley@gmail.com>
2026-07-04 10:39:57 -0600

    tested spinner method-- working as intended :]

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
-    }
+    
 }
+
+/* static void Main(string[] args)
+    {
+        Activity test = new Activity("test", "testing spinner");
+
+        Console.WriteLine("Test spinner for 2 seconds");
+        test.Spinner(5);
+        Console.WriteLine();
+        Console.WriteLine("Test complete");
+    } */
