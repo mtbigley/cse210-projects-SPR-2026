093fe53cf7798fabad78becacaace8b84902a843
mtbigley <mtbigley@byui.edu>
2026-07-16 05:43:14 -0600

    Edit class info to resemble test outputs, initial commit of Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,15 +6,20 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
 
-        Job job = new Job(); 
-        job._jobTitle = " ooblar ";
-        job._company = "blarblar";
+        Job job1 = new Job(); 
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022; 
 
         Job job2 = new Job(); 
-        job2._jobTitle = "blargablarg";
-        job2._company = "floogaboogla";
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        job.DisplayJobDetails();
+
+        job1.DisplayJobDetails();
         job2.DisplayJobDetails();
 
         // Console.WriteLine($"{job._jobTitle}");
