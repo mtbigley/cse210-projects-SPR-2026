aad95f09bed450b3f629c7c4a9af30562b9c6520
mtbigley <mtbigley@byui.edu>
2026-07-16 15:34:53 -0600

    initial commit of Entry.cs

---
+++ Entry.cs
@@ -0,0 +1,15 @@
+using System; 
+
+public class Entry
+{
+    public string _date; 
+    public string _prompt;
+    public string _entry; 
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_prompt}");
+        Console.WriteLine($"Text: {_entry}");
+    }
+}
