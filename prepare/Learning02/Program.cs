8cb8ca29165380b768b74bcabb34d0f5ef335c92
mtbigley <mtbigley@byui.edu>
2026-07-16 05:34:59 -0600

    Display Job Details method and test commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,12 +6,16 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
 
-        // Job job = new Job(); 
-        // job._jobTitle = " ooblar ";
-        // job._company = "blarblar";
+        Job job = new Job(); 
+        job._jobTitle = " ooblar ";
+        job._company = "blarblar";
 
-        // Job job2 = new Job(); 
-        // job2._jobTitle = "blargablarg";
+        Job job2 = new Job(); 
+        job2._jobTitle = "blargablarg";
+        job2._company = "floogaboogla";
+
+        job.DisplayJobDetails();
+        job2.DisplayJobDetails();
 
         // Console.WriteLine($"{job._jobTitle}");
         // Console.WriteLine($"{job2._jobTitle}");
