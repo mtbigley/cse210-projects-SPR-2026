965e5a82363489f3bcf9b0771dc0c75db77bba0a
mtbigley <mtbigley@byui.edu>
2026-07-18 05:24:33 -0600

    initial commit for Goal.cs class with variables, constructor and getters

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,25 @@
+using System; 
+
+public abstract class Goal
+{
+    private string _name;
+    private string _description; 
+    private int _points; 
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name; 
+        _description = description;
+        _points = points; 
+    }
+
+    public string Getname()
+    {
+        return _name;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+}
