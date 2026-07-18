a555068c697dc055f78e6b85e204b7cf77f17f94
mtbigley <mtbigley@byui.edu>
2026-07-18 14:59:28 -0600

    initial commit for ChecklistGoal.cs-- this one was trickier and took me a bit but I got it where I want it for now.

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,48 @@
+using System; 
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target; 
+    private int _bonus;
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        _target = target; 
+        _bonus = bonus; 
+    }
+
+    public override int RecordEvent()
+    {
+        if (IsComplete() == true)
+        {
+            return 0;
+        }
+        
+        _amountCompleted++; 
+        
+        if (_amountCompleted == _target)
+        {
+            int finalPoints = GetPoints() + _bonus;
+            return finalPoints;
+        }
+
+        else
+        {
+            int regularPoints = GetPoints(); 
+            return regularPoints; 
+        }
+    }
+
+    public override bool IsComplete()
+    {
+        if (_amountCompleted >= _target)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false; 
+        }
+    }
+}
