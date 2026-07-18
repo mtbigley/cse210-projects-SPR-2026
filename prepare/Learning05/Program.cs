784e5d71ae63b4848064a86acdfa7b6d2170f41b
mtbigley <mtbigley@byui.edu>
2026-07-18 03:57:38 -0600

    initial commit of circle class.cs and tests completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Square testsquare = new Square("Blue", 5); 
+        Circle testcircle = new Circle("Red", 14);
 
-        
     }
 }
 
 // Console.WriteLine(testsquare.GetArea());
 // Console.WriteLine(testsquare.GetColor());
+
+// Console.WriteLine(testcircle.GetArea());
+// Console.WriteLine(testcircle.GetColor());
