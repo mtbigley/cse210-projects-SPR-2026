220ea57786b2002c782263270ed673dc20af2f89
mtbigley <mtbigley@byui.edu>
2026-07-21 17:03:15 -0600

    initial commit for address.cs

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,22 @@
+using System; 
+// reused a lot of this architecture from Foundation 2's handling of addresses. 
+public class Address
+{
+    private string _street; 
+    private string _city; 
+    private string _state; 
+    private string _country; 
+
+    public Address(string street, string city, string state, string country)
+    {
+        _street = street; 
+        _city = city; 
+        _state = state; 
+        _country = country; 
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_street}, {_city}, {_state}, {_country}";
+    }
+}
